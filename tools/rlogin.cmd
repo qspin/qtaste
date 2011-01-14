5f1b6c846c4a9847faf2d15e32327f1fddfba87e
Laurent Vanboquestal <lvb@qspin.be>
2011-01-14 14:28:05 +0000

    Updating path in script file

--- tools/rlogin.cmd
+++ tools/rlogin.cmd
@@ -1,10 +1,10 @@
 @echo off
-rem rlogin client using the com.iba.ate.tcom.rlogin.RLogin class
+rem rlogin client using the com.qspin.qtaste.tcom.rlogin.RLogin class
 rem Usage: <command> <remote_host> [-user <user>] [-reboot | -command <command>] [-logOutput] [-wait [seconds]] [-log4jconf <properties_file>]
 
 setlocal
 
 cd %~dp0\..
-java -cp kernel/target/ate-kernel-deploy.jar com.iba.ate.tcom.rlogin.RLogin %*|| exit 1
+java -cp kernel/target/qtaste-kernel-deploy.jar com.qspin.qtaste.tcom.rlogin.RLogin %*|| exit 1
 
 endlocal
