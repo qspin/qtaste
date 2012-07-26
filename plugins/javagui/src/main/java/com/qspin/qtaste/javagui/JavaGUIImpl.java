59334295151fc789e19aecd88544c31e85c09a74
Simon Janssens <sja@qspin.be>
2012-07-26 09:18:41 +0000

    Default JAVAGUI controller

--- plugins/javagui/src/main/java/com/qspin/qtaste/javagui/JavaGUIImpl.java
+++ plugins/javagui/src/main/java/com/qspin/qtaste/javagui/JavaGUIImpl.java
@@ -11,12 +11,13 @@ import com.qspin.qtaste.tcom.jmx.impl.JMXClient;
  * 
  */
 public abstract class JavaGUIImpl extends JMXClient implements
-		MultipleInstancesComponent {
+		MultipleInstancesComponent, JavaGUIMBean {
 
 	public JavaGUIImpl(String url, String instanceId) throws Exception {
 		super(url);
 		mInstanceId = instanceId;
 		connect();
+		mProxy = getProxy();
 	}
 
 	@Override
@@ -24,6 +25,72 @@ public abstract class JavaGUIImpl extends JMXClient implements
 		return mInstanceId;
 	}
 
-	protected String mInstanceId;
+	public boolean clickOnButton(String pComponentName) {
+		return mProxy.clickOnButton(pComponentName);
+	}
 
+	public void takeSnapShot(String componentName, String fileName)
+	{
+		mProxy.takeSnapShot(componentName, fileName);		
+	}
+
+	public boolean keyPressedOnComponent(String componentName, int vkEvent)
+	{
+		return mProxy.keyPressedOnComponent(componentName, vkEvent);
+	}
+
+	public String[] listComponents()
+	{
+		return mProxy.listComponents();
+	}
+
+	public boolean isEnabled(String componentName)
+	{
+		return mProxy.isEnabled(componentName);
+	}
+
+	public boolean clickOnButton(String componentName, int pressTime)
+	{
+		return mProxy.clickOnButton(componentName, pressTime);
+	}
+
+	public String getText(String componentName)
+	{
+		return mProxy.getText(componentName);
+	}
+
+	public boolean setText(String componentName, String value)
+	{
+		return mProxy.setText(componentName, value);
+	}
+
+	public boolean selectComponant(String componentName, boolean value)
+	{
+		return mProxy.selectComponant(componentName, value);
+	}
+
+	public boolean selectValue(String componentName, String value)
+	{
+		return mProxy.selectValue(componentName, value);
+	}
+
+	public boolean selectIndex(String componentName, int index)
+	{
+		return mProxy.selectIndex(componentName, index);
+	}
+
+	public boolean selectNode(String componentName, String nodeName,
+			String nodeSeparator)
+	{
+		return mProxy.selectNode(componentName, nodeName, nodeSeparator);
+	}
+
+	private JavaGUIMBean getProxy() throws Exception {
+		return (JavaGUIMBean) getProxy(BEAN_NAME, BEAN_INTERFACE);
+	}
+
+	protected String mInstanceId;
+	protected JavaGUIMBean mProxy;
+	private static final String BEAN_NAME = "com.qspin.qtaste.javagui:type=JavaGUI";
+	private static final Class<?> BEAN_INTERFACE = JavaGUIMBean.class;
 }
