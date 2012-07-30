aa1021530bb5414a9cf0dcea7e971f1264280a17
Simon Janssens <sja@qspin.be>
2012-07-30 13:49:59 +0000

    [Kernel] return 1 if test failed

--- bin/qtaste_start.bat
+++ bin/qtaste_start.bat
@@ -2,5 +2,9 @@
 setlocal
 set QTASTE_ROOT=%~dp0\..
 set PATH=%PATH%;%QTASTE_ROOT%\lib
-java -Xms64m -Xmx512m -cp "%QTASTE_CLASSPATH%";%QTASTE_ROOT%/plugins/*;%QTASTE_ROOT%/kernel/target/qtaste-kernel-deploy.jar;testapi/target/qtaste-testapi-deploy.jar com.qspin.qtaste.kernel.engine.TestEngine %*
+java -Xms64m -Xmx512m -cp "%QTASTE_CLASSPATH%";%QTASTE_ROOT%/plugins/*;%QTASTE_ROOT%/kernel/target/qtaste-kernel-deploy.jar;testapi/target/qtaste-testapi-deploy.jar com.qspin.qtaste.kernel.engine.TestEngine %* | set result=1
+if %result% == 1
+then
+    echo test failed
+fi
 endlocal
