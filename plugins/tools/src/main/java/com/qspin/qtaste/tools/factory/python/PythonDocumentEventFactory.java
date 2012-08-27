d859381c5101c13f167a0c138b7420b1ad8b0d81
Simon Janssens <sja@qspin.be>
2012-08-27 11:48:35 +0000

    [PLUGIN-TOOLS] bug fix - Management of the line separator character

--- plugins/tools/src/main/java/com/qspin/qtaste/tools/factory/python/PythonDocumentEventFactory.java
+++ plugins/tools/src/main/java/com/qspin/qtaste/tools/factory/python/PythonDocumentEventFactory.java
@@ -6,16 +6,19 @@ import com.qspin.qtaste.tools.model.event.Event;
 class PythonDocumentEventFactory extends PythonEventFactory {
 
 	@Override
-	public String createPythonEvent(Event pEvent, long pPreviousTimestamp)
-	{
-		DocumentEvent evt = (DocumentEvent)pEvent;
+	public String createPythonEvent(Event pEvent, long pPreviousTimestamp) {
+		DocumentEvent evt = (DocumentEvent) pEvent;
 		StringBuilder builder = new StringBuilder();
 
-		if ( !evt.getChange().equals("\n") && !evt.getChange().equals(LINE_BREAK))
-		{
-			insertSleep(pEvent, pPreviousTimestamp, builder);
-			builder.append(getPythonIndentation(1) + "javaguiMI.setText(" + getComponentIdentifier(evt.getComponentName()) + ", \"" + evt.getChange() + "\")" + LINE_BREAK);
+		String command = "javaguiMI.setText(" + getComponentIdentifier(evt.getComponentName()) + ", \"";
+		if (!evt.getChange().contains(LINE_BREAK)) {
+			 command += evt.getChange();
+		} else {
+			command += evt.getChange().replace(LINE_BREAK, "\\n");
 		}
+		command += "\")";
+		insertSleep(pEvent, pPreviousTimestamp, builder);
+		builder.append(getPythonIndentation(1) + command + LINE_BREAK);
 		return builder.toString();
 	}
 
