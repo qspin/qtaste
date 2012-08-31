f756b14e486eb4a345adc7f5ca467aa1b89824b9
Simon Janssens <sja@qspin.be>
2012-08-31 14:56:33 +0000

    [DEMO] PLAYBACK demonstration

---
+++ demo/Testbeds/ControlScripts/playback.py
@@ -0,0 +1,11 @@
+from controlscript_addon import *
+
+ControlScript([
+	JavaProcess( "PlayBack-Interface",
+	mainClassOrJar="com.qspin.qtaste.testapi.ui.Interface",
+	workingDir="demo",
+	classPath="testapi/target/qtaste-testapi-deploy.jar",
+	vmArgs="-javaagent:../plugins/javagui/target/qtaste-javagui-deploy.jar",
+	jmxPort=10101,
+	checkAfter=2)
+])
