e6c1ee79305122c2babd2aa87d1547be5219986c
Simon Janssens <sja@qspin.be>
2012-09-19 13:01:50 +0000

    remove old packaging scripts

--- packager_qtaste_demo.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set QTASTE_ROOT=%~dp0
-rem delete testapi target directory because mvn cannot always delete it when run from jython
-rmdir /s /q demo\testapi\target 2>NUL
-call %QTASTE_ROOT%\tools\jython\bin\jython.bat %QTASTE_ROOT%\packager_qtaste_demo.py
-pause
--- packager_qtaste_kernel.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set QTASTE_ROOT=%~dp0
-cd %QTASTE_ROOT%
-rem delete kernel/target directory because mvn cannot always delete it when run from jython
-rmdir /s /q kernel\target 2>NUL
-call tools\jython\bin\jython.bat packager_qtaste_kernel.py
-pause
