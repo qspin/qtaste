5a5fd73b4bf92e1d413d0bd2fdea0b62f382349d
Simon Janssens <sja@qspin.be>
2012-09-24 10:27:57 +0000

    [PLUGIN-TOOLS] code cleaning

--- plugins/tools/src/main/java/com/qspin/qtaste/tools/factory/python/PythonEventFactoryInterface.java
+++
@@ -1,9 +0,0 @@
-package com.qspin.qtaste.tools.factory.python;
-
-import com.qspin.qtaste.tools.model.event.Event;
-
-
-public interface PythonEventFactoryInterface {
-
-	String createPythonEvent(Event pEvent, long pPreviousTimestamp);
-}
