3b23b41e86a043d10ab8aabe2b2221276db68d55
Simon Janssens <sja@qspin.be>
2012-09-27 11:48:29 +0000

    [DEMO] bug fix

--- demo/testapi/src/main/java/com/qspin/qtaste/testapi/api/Playback.java
+++ demo/testapi/src/main/java/com/qspin/qtaste/testapi/api/Playback.java
@@ -1,6 +1,6 @@
 package com.qspin.qtaste.testapi.api;
 
-import com.qspin.qtaste.javagui.JavaGUIMBean;
+import com.qspin.qtaste.javagui.server.JavaGUIMBean;
 import com.qspin.qtaste.kernel.testapi.MultipleInstancesComponent;
 
 public interface Playback extends JavaGUIMBean, MultipleInstancesComponent{
