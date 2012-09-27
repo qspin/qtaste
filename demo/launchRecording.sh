68bf23ba5d8354bbf2d7ac8aaa0acfcb837dd563
Simon Janssens <sja@qspin.be>
2012-09-27 14:51:42 +0000

    [DEMO] filter xml files samples / [RECORDER] missing java file for filter fonctionality

--- demo/launchRecording.sh
+++ demo/launchRecording.sh
@@ -3,8 +3,8 @@
 
 ROOT=`pwd`/../
 RECORDER_PATH=$ROOT/plugins/recorder
-
+FILTER_PATH="=./recordingFilter.xml"
 echo 
-java -javaagent:$RECORDER_PATH/target/qtaste-recorder-deploy.jar -cp testapi/target/qtaste-testapi-deploy.jar com.qspin.qtaste.testapi.ui.Interface || exit 1
+java -javaagent:$RECORDER_PATH/target/qtaste-recorder-deploy.jar$FILTER_PATH -cp testapi/target/qtaste-testapi-deploy.jar com.qspin.qtaste.sutuidemo.Interface || exit 1
 
 echo "An XML file named spyRepport_*.xml has been created with all recorded events. You can use the tools plugin to convert it into a python script usable with QTaste in order to reproduce your actions."
