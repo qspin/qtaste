80bc94407a90b4932676590ef0b48485f64a9acc
Simon Janssens <sja@qspin.be>
2012-10-10 06:50:10 +0000

    [KERNEL] add windows script to start/stop a service

---
+++ tools/stop_service_process.cmd
@@ -0,0 +1,19 @@
+@echo off
+rem stop a  process and wait for its end
+rem usage: stop_service_process <serviceName> 
+
+setlocal enableDelayedExpansion 
+
+set SERVICE_NAME=%~1
+
+net stop %SERVICE_NAME%
+
+call %~dp0\get_service_status %SERVICE_NAME%
+
+if %errorlevel% == 0 (
+	echo Service %SERVICE_NAME% is stopped
+	exit 0
+) else (
+	echo Service %SERVICE_NAME% has not been stopped
+	exit 1
+)
