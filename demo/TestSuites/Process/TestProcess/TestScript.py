071e3614bae1990171c18add75e93d035ded22d6
Simon Janssens <sja@qspin.be>
2013-01-09 15:38:48 +0000

    [DEMO] fix problems on process demonstration

--- demo/TestSuites/Process/TestProcess/TestScript.py
+++ demo/TestSuites/Process/TestProcess/TestScript.py
@@ -1,32 +1,47 @@
 ##
 # TestProcess.
 # <p>
-# Description of the test.
+# <u><b>Test only available on LINUX.</b></u>
+# <p>
+# Try to launch the xterm command and then stop the created process.
 #
-# @data INSTANCE_ID [String] instance id
 ##
 
 from qtaste import *
 from com.qspin.qtaste.testapi.api import ProcessStatus
 import time
 
-def step1():
+
+process = testAPI.getLinuxProcess(INSTANCE_ID="p1")
+
+def initializeProcess():
 	"""
-	@step      Description of the actions done for this step
-	@expected  Description of the expected result
+	@step      Initialize the xterm process
+	@expected  The process status is READY_TO_START
 	"""
-	process = testAPI.getLinuxProcess(INSTANCE_ID="p1")
 	logger.info(process.getStatus())
 	checkStatus(process.getStatus(), ProcessStatus.UNDEFINED)
-	params = ["java","-cp", "/home/sjansse/workspaces/qtaste/demo/testapi/target/qtaste-testapi-deploy.jar","com.qspin.qtaste.sutuidemo.Interface"]
+	params = ["xterm"]
 	process.initialize(params)
 	checkStatus(process.getStatus(), ProcessStatus.READY_TO_START)
+	
+def launchProcess():
+	"""
+	@step      Launch the xterm process
+	@expected  The process status is RUNNING
+	"""
 	logger.info(process.getStatus())
 	process.start()
 	time.sleep(2)
 	logger.info(process.getStatus())
 	checkStatus(process.getStatus(), ProcessStatus.RUNNING)
 	logger.info("Process pid : " + str(process.getPid()))
+	
+def stopProcess():
+	"""
+	@step      Stop the process
+	@expected  The process status is STOPPED
+	"""
 	process.stop()
 	time.sleep(2)
 	logger.info(process.getStatus())
@@ -41,4 +56,6 @@ def checkStatus(currentStatus, expectedStatus):
 	if currentStatus != expectedStatus:
 		testAPI.stopTest(Status.FAIL, "The process has not the expected status. Current status is " + currentStatus.name() + " in state of " + expectedStatus.name())
 	
-doStep(step1)
+doStep(initializeProcess)
+doStep(launchProcess)
+doStep(stopProcess)
