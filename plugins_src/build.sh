c1d1fd804bfd2e1273347edb2301117fd002aadf
Simon Janssens <sja@qspin.be>
2013-01-11 14:25:48 +0000

    [PLUGINS] copy jar in the plugins/SUT directory after a build

--- plugins_src/build.sh
+++ plugins_src/build.sh
@@ -2,10 +2,16 @@
 
 PLUGINS="tools javagui recorder"
 
+targetDir=../plugins/SUT
+if [ ! -d "$targetDir" ]
+then
+    mkdir $targetDir
+fi
+
 for PLUGIN in $PLUGINS
 do
 	pushd $PLUGIN
 	mvn clean install assembly:single || exit 1
-	cp target/*-deploy.jar ../../plugins/
+	cp target/*-deploy.jar ../$targetDir
 	popd
 done
