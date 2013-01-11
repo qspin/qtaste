685a809fadc09c7d124f2a21bbf08069fa306b8b
Simon Janssens <sja@qspin.be>
2013-01-11 13:17:31 +0000

    [ControlScriptBuilderAddOn] code cleaning

--- plugins_src/ControlScriptBuilderAddOn/src/main/java/com/qspin/qtaste/controlscriptbuilder/io/ControlScriptDecoder.java
+++ plugins_src/ControlScriptBuilderAddOn/src/main/java/com/qspin/qtaste/controlscriptbuilder/io/ControlScriptDecoder.java
@@ -34,13 +34,11 @@ public final class ControlScriptDecoder {
 		InputStream is = null;
 		try
 		{
-			LOGGER.info("Start reading");
 			is = new FileInputStream(pControlScriptFile);
 			mParameters.load(is);
 		}
 		finally
 		{
-			LOGGER.info("Stop reading.");
 			IOUtils.closeQuietly(is);
 		}
 	}
@@ -74,60 +72,6 @@ public final class ControlScriptDecoder {
 		return new ArrayList<ControlAction>(mapAction.values());
 	}
 	
-//	private List<ControlAction> parseControlScriptDataLine()
-//	{
-//		List<ControlAction> actions = new ArrayList<ControlAction>();
-//		if ( mLineToParse == null)
-//			return actions;
-//		mLineToParse = mLineToParse.trim();
-//		LOGGER.info("will parse : " + mLineToParse);
-//		//divide by process type
-//		while ( !mLineToParse.isEmpty() )
-//		{
-//			mLineToParse = mLineToParse.trim();
-//			//search the process type
-//			String processType = mLineToParse.substring(0, mLineToParse.indexOf("("));
-//			mLineToParse = mLineToParse.substring(processType.length()+1);
-//			//clean the process type
-//			processType = processType.trim();
-//			if (processType.startsWith(","))
-//				processType = processType.substring(1);
-//			processType = processType.trim();
-//			
-//			//get process parameters
-//			int parCpt = 1;
-//			String params = "";
-//			for ( int i=0; parCpt > 0; i++ )
-//			{
-//				char c = mLineToParse.charAt(i);
-//				if ( c == '(' )
-//					parCpt ++;
-//				else if ( c == ')' )
-//					parCpt --;
-//				params += c;
-//			}
-//			
-//			mLineToParse = mLineToParse.substring(params.length());
-//			//remove last )
-//			params = params.substring(0, params.lastIndexOf(')'));
-//			
-//			LOGGER.info("process found !");
-//			LOGGER.info("process type : " + processType);
-//			LOGGER.info("process parameters : " + params);
-//			
-//			ControlAction action = ControlActionFactory.createControlAction(processType, params);
-//			LOGGER.info("Control action created : " + action);
-//			if ( action != null )
-//			{
-//				actions.add(action);
-//			}
-//		}
-//		return actions;
-//	}
-
-//	private String mLineToParse; 
-//	private int mBracketCounter = -1;
-//	private int mParentheseCounter = -1;
 	private Properties mParameters;
 	
 	private static final Logger LOGGER = Logger.getLogger(ControlScriptDecoder.class);
