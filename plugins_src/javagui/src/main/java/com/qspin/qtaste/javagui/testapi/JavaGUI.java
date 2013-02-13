77ca65a09bb98c07fdaab08ed11410b69015ca28
Laurent Vanboquestal <lvb@qspin.be>
2013-02-13 11:26:32 +0000

    [PLUGIN/DEMO] TestApi is not needed to use javagui + adapt demo

---
+++ plugins_src/javagui/src/main/java/com/qspin/qtaste/javagui/testapi/JavaGUI.java
@@ -0,0 +1,7 @@
+package com.qspin.qtaste.javagui.testapi.api;
+
+import com.qspin.qtaste.kernel.testapi.MultipleInstancesComponent;
+
+public interface JavaGUI extends com.qspin.qtaste.javagui.JavaGUI, MultipleInstancesComponent{
+
+}
