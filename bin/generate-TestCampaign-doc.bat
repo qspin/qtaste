2b8b4c40d7f4006ec9ed2edf8dd659a311f267ce
Simon Janssens <sja@qspin.be>
2013-04-18 07:38:53 +0000

    [SCRIPT] use the java implementation to generate test campaign documentation

--- bin/generate-TestCampaign-doc.bat
+++ bin/generate-TestCampaign-doc.bat
@@ -1,19 +1,8 @@
 @echo off
 setlocal enableDelayedExpansion 
 
-call %~dp0\setenv
-echo Generating documentation of test suites included in test campaign
-echo.
+set QTASTE_ROOT=%~dp0\..
+set Path=%Path%;%QTASTE_ROOT%\lib
+java -Xms64m -Xmx512m -cp %QTASTE_ROOT%\plugins\*;%QTASTE_ROOT%\kernel\target\qtaste-kernel-deploy.jar com.qspin.qtaste.util.GenerateTestCampaignDoc 2>&1 %*
 
-set TEMP_FILE=%TEMP%\generateTestCampaignDoc.tmp
-del %TEMP_FILE% >NUL 2>&1
-for /f %%i in ('type %1 ^| find "<testsuite " ^| %QTASTE_ROOT%\tools\GnuWin32\bin\sed -r -e "s/^.*testsuite\s*directory=\"^(TestSuites^(\/^^^|\\^)[^^^^/\\]*^).*\"\s*\/?>\s*$/\1/" ^| sort ') do @(if not "%%i" == "!previous!" (echo %%i) & set previous=%%i) >> %TEMP_FILE%
-echo Found test suites:
-type %TEMP_FILE%
-echo.
-echo.
-for /f %%i in (%TEMP_FILE%) do call %~dp0\generate-TestSuite-doc %%i > log.txt
-
-echo.
-call %QTASTE_ROOT%\tools\jython\bin\jython %QTASTE_ROOT%\tools\TestProcedureDoc\generateTestCampaignDoc.py %1
 endlocal
