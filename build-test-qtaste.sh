e75010988c4f62dc3488883f79742d241bb3aa23
nfac <nco@qspin-fixe2.qspin.be>
2013-07-17 10:48:26 +0200

    Added script to buil and test qtaste (Demo). This script is meant to be used by CloudBees Platform for Continuous Integration operations

---
+++ build-test-qtaste.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+echo "Executing script build-test-qtaste"
+
+# Configurations:
+export DISPLAY=:99.0
+Xvfb :99.0 &
+
+# Build:
+./buildAll.sh || exit 1
+
+# Execute qtaste demo after build success:
+# ./executeDemo.sh || exit 1
+
