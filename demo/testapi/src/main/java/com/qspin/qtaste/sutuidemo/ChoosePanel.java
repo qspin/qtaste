96245b89b9dffd0dd995f6cf468994555b244661
SimJan <sja@qspin.be>
2013-08-20 15:03:55 +0200

    [DEMO] add a new component in order to test JToggleButton

--- demo/testapi/src/main/java/com/qspin/qtaste/sutuidemo/ChoosePanel.java
+++ demo/testapi/src/main/java/com/qspin/qtaste/sutuidemo/ChoosePanel.java
@@ -6,6 +6,7 @@ import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JToggleButton;
 
 final class ChoosePanel extends JPanel {
 
@@ -26,18 +27,22 @@ final class ChoosePanel extends JPanel {
 		add(mCheck);
 		add(new JLabel("JRadioButton :"));
 		add(mRadio);
+		add(new JLabel("JToggleButton :"));
+		add(mToggle);
 	}
 	
 	private void prepareComponent()
 	{
 		mCheck.setName("CHECK_BOX");
 		mRadio.setName("RADIO_BUTTON");
+		mToggle.setName("TOGGLE_BUTTON");
 	}
 
 	private JCheckBox mCheck = new JCheckBox("Check box");
 	private JRadioButton mRadio = new JRadioButton("Radio");
+	private JToggleButton mToggle = new JToggleButton("Toggle", false);
 	
 	
-	private static final int NUMBER_OF_COMPONENT = 2;
+	private static final int NUMBER_OF_COMPONENT = 3;
 	public static final String COMPONENT_NAME = "CHOOSE_PANEL";
 }
