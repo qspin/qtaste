0162f057faf7851801b72ba3dcb2704ce979c6a8
SimJan <sja@qspin.be>
2013-09-05 15:14:46 +0200

    [DEMO] issue #58 - remove dupplicate testapi class

--- demo/testapi/src/main/java/com/qspin/qtaste/testapi/api/Playback.java
+++
@@ -1,8 +0,0 @@
-package com.qspin.qtaste.testapi.api;
-
-import com.qspin.qtaste.javagui.JavaGUI;
-import com.qspin.qtaste.kernel.testapi.MultipleInstancesComponent;
-
-public interface Playback extends JavaGUI, MultipleInstancesComponent{
-
-}
