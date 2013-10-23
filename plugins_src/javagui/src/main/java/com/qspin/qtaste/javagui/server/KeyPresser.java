f4d6650063411a4cdda9af984537a43bd7f3e3d6
“lvboque” <lvb@qspin.be>
2013-10-23 20:43:10 +0200

    Add (c) header in all files related to JavaGUI

--- plugins_src/javagui/src/main/java/com/qspin/qtaste/javagui/server/KeyPresser.java
+++ plugins_src/javagui/src/main/java/com/qspin/qtaste/javagui/server/KeyPresser.java
@@ -16,7 +16,9 @@
 
     You should have received a copy of the GNU Lesser General Public License
     along with QTaste. If not, see <http://www.gnu.org/licenses/>.
-*/package com.qspin.qtaste.javagui.server;
+*/
+
+package com.qspin.qtaste.javagui.server;
 
 import java.awt.Robot;
 
