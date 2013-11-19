e106ca5b03805675ac0984ab43c9c8820e6a05ea
Laurent Vanboquestal <lvb@qspin.be>
2013-11-19 11:31:30 +0100

    Fix a bug introduced when changing the signature in the Snapshotter

--- plugins_src/javagui/src/main/java/com/qspin/qtaste/javagui/server/Snapshotter.java
+++ plugins_src/javagui/src/main/java/com/qspin/qtaste/javagui/server/Snapshotter.java
@@ -39,13 +39,13 @@ class Snapshotter extends UpdateComponentCommander {
 			Graphics2D g2 = myImage.createGraphics();
 			component.paint(g2);
 	
-			File file = new File(mData[2].toString());
+			File file = new File(mData[0].toString());
 			file.createNewFile();
 			System.out.println("creating empty file");
 			ImageIO.write(myImage, "jpg", file);				 			
 		}
 		catch (Exception e) {
-			throw new QTasteTestFailException("Error saving snapshot " + mData[2].toString() + ":", e);
+			throw new QTasteTestFailException("Error saving snapshot " + mData[0].toString() + ":", e);
 		}	
 	}
 
