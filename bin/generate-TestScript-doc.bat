11764be5aa703a35ee9e8f83d6351a9e688df92a
simjan <janssenssimonjc@gmail.com>
2014-03-12 11:53:22 +0100

    [KERNEL] issue #92 - update bat script to use the JAVA code

--- bin/generate-TestScript-doc.bat
+++ bin/generate-TestScript-doc.bat
@@ -15,38 +15,6 @@ goto end
 
 :begin
 
-setlocal
-
-set TEST_SCRIPT=%~f1
-set TEST_SCRIPT_DIR=%~dp1
-echo Test script: %TEST_SCRIPT%
-
-if not exist %TEST_SCRIPT% (
-  echo File %TEST_SCRIPT% doesn't exist
-  goto end
-)
-
-call %~dp0\setenv
-
-rem generate Test steps modules doc from top-level TestSuites directory
-call %QTASTE_ROOT%\bin\generate-TestStepsModules-doc TestSuites
-
-set FORMATTER_DIR="%QTASTE_ROOT%\tools\TestScriptDoc"
-
-echo.
-echo Generating Test script XML doc...
-java -cp %JYTHON_HOME%/jython.jar;%QTASTE_ROOT%/kernel/target/qtaste-kernel-deploy.jar;testapi\target\qtaste-testapi-deploy.jar -Dpython.home=%JYTHON_HOME% -Dpython.path=%FORMATTER_DIR% org.python.util.jython %JYTHON_HOME%\Lib\pythondoc.py -f -s -Otestscriptdoc_xmlformatter %TEST_SCRIPT%
-echo.
-
-if exist %TEST_SCRIPT_DIR%\TestScript-doc.xml (
-  echo Converting Test script XML doc to HTML...
-  java -cp %QTASTE_ROOT%/kernel/target/ate-kernel-deploy.jar org.apache.xalan.xslt.Process -XSLTC -XT -IN %TEST_SCRIPT_DIR%\TestScript-doc.xml -XSL %FORMATTER_DIR%\testscriptdoc_xml2html.xsl -OUT %TEST_SCRIPT_DIR%\TestScript-doc.html
-  del %TEST_SCRIPT_DIR%\TestScript-doc.xml
-) else (
-  echo XML test script doc has not been generated for %TEST_SCRIPT%!
-  del !TEST_SCRIPT_DIR!\TestScript-doc.html 2>NUL
-)
-
-endlocal
-
-:end
+set QTASTE_ROOT=%~dp0\..
+set Path=%Path%;%QTASTE_ROOT%\lib
+java -Xms64m -Xmx512m -cp %QTASTE_ROOT%\plugins\*;%QTASTE_ROOT%\kernel\target\qtaste-kernel-deploy.jar;testapi\target\qtaste-testapi-deploy.jar com.qspin.qtaste.util.GenerateTestScriptDoc %*
