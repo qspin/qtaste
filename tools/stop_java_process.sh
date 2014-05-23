5fe138864e12fdf025b4c7f064dfa4aa709bb459
David Ergo <david.ergo@iba-group.com>
2014-05-23 14:19:09 +0200

    [TOOLS] Fixed issue #106: process was not stopped when command line contained trailing whitespaces

--- tools/stop_java_process.sh
+++ tools/stop_java_process.sh
@@ -4,6 +4,13 @@
 # usage: stop_java_process <main_class_and_args> 
 
 JP_NAME=$1
+
+# trim leading and trailing whitespaces
+shopt -s extglob
+JP_NAME="${JP_NAME##*( )}"
+JP_NAME="${JP_NAME%%*( )}"
+shopt -u extglob
+
 echo "Stopping $JP_NAME"
 
 INFO=$(ps -fu $USER | grep "$JP_NAME" | grep -v "grep" | grep -v "stop_java_process")
--- tools/stop_process.sh
+++ tools/stop_process.sh
@@ -4,6 +4,13 @@
 # usage: stop_process <process> 
 
 P_NAME=$1
+
+# trim leading and trailing whitespaces
+shopt -s extglob
+P_NAME="${P_NAME##*( )}"
+P_NAME="${P_NAME%%*( )}"
+shopt -u extglob
+
 echo "Stopping $P_NAME"
 
 INFO=$(ps -fu $USER | grep "$P_NAME" | grep -v "grep" | grep -v "stop_process")
@@ -29,5 +36,4 @@ for(( i = 0 ; i < 5 ; i++ )) do
    if [ ! -z $PID ]; then
       kill -9 $PID
    fi
-
 fi
