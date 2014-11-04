c910a9f3b0a4f3f25de0c03e2ad1abf0bb0f50f3
lvboque <laurent.vanboquestal@gmail.com>
2014-11-04 15:50:39 +0100

    #146 : simplify a bit the example

--- demo_opc/TestSuites/pythonlib/Common.py
+++ demo_opc/TestSuites/pythonlib/Common.py
@@ -7,19 +7,6 @@ from qtaste import *
 import time
 import string
 
-def goToMaintenanceMode(opc):
-    opc.write("SUP_WR_BIN_AC_MAINTENANCE", "1")
-    actualValue = opc.readInt("SUP_RO_INT_ST_GENERAL_STATUS")
-    if not (actualValue == 0):
-        testAPI.stopTest(Status.FAIL,"Cannot go to maintenance Mode: got "+ str(actualValue) +" for variable SUP_RO_INT_ST_GENERAL_STATUS")
-
-def switchOnCompressionAir(opc):
-    opc.write("SUP_WR_BIN_DO_COMP_AIR_SUPPLY", "1")
-    time.sleep(1)    
-    actualValue = opc.readInt("IO_DO_COMP_AIR_SUPPLY")
-    if not (actualValue == 1):
-        testAPI.stopTest(Status.FAIL,"Cannot switch ON Compression Air!")
-
 def checkVarIntSync(opc, spVariable, spValue, expectedValue, readVariable, delay, precision=0):
     opc.write(spVariable, str(spValue))
     time.sleep(delay)
