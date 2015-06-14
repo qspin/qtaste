a2f72a287957be07e7a8a165298a2fadc7a35821
Remy Baranx <remy.bricol@gmail.com>
2015-06-14 16:47:43 +0200

    BUG#178: GUI methods are now called in the EDT

--- demo/testapi/src/main/java/com/qspin/qtaste/testapi/impl/demo/SubtitlerImpl.java
+++ demo/testapi/src/main/java/com/qspin/qtaste/testapi/impl/demo/SubtitlerImpl.java
@@ -8,6 +8,7 @@ import java.awt.Toolkit;
 import javax.swing.JLabel;
 import javax.swing.JWindow;
 import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 
 import com.qspin.qtaste.testapi.api.Subtitler;
 import com.qspin.qtaste.testsuite.QTasteException;
@@ -18,83 +19,143 @@ public final class SubtitlerImpl implements Subtitler, Runnable {
 	 * Constructor.
 	 * @throws QTasteException
 	 */
-	public SubtitlerImpl() throws QTasteException
-	{
-		super();
-		
-		int width = Toolkit.getDefaultToolkit().getScreenSize().width;
-		int height = 100;
-		
-		m_subtitleFrame.setPreferredSize(new Dimension(width, height));
-		m_subtitleFrame.setLocation(0, Toolkit.getDefaultToolkit().getScreenSize().height - height);
-		m_subtitleFrame.setBackground(null);
-		
-		m_subtitleFrame.setLayout(new GridLayout());
-		m_subtitle = new JLabel();
-		Font f = new Font(Font.SERIF, Font.PLAIN, 20);
-		m_subtitle.setFont(f);
-		m_subtitle.setHorizontalAlignment(SwingConstants.CENTER);
-		m_subtitleFrame.add(m_subtitle);
+	public SubtitlerImpl() throws QTasteException {
+		super();		
 
-		m_subtitleFrame.pack();
-		
-		initialize();
-	}
-
-	@Override
-	public void initialize() throws QTasteException
-	{
-		m_abort = false;
-		m_thread = new Thread(this);
-		m_thread.start();
-	}
-
-	@Override
-	public void terminate() throws QTasteException {
-		m_subtitleFrame.setVisible(false);
-		m_abort = true;
-	}
-
-	@Override
-	public void setSubtitle(String subtitle) {
-		setSubtitle(subtitle, 3);
-	}
+		// window initialization in the EDT
+		try {
+			SwingUtilities.invokeAndWait(new Runnable() {
+				public void run() {
+					int width = Toolkit.getDefaultToolkit().getScreenSize().width;
+					int height = 100;
+					
+					m_subtitleFrame = new JWindow();
+					
+					m_subtitleFrame.setPreferredSize(new Dimension(width, height));
+					m_subtitleFrame.setLocation(0, Toolkit.getDefaultToolkit().getScreenSize().height - height);
+					m_subtitleFrame.setBackground(null);
+					
+					m_subtitleFrame.setLayout(new GridLayout());
+					m_subtitle = new JLabel();
+					Font f = new Font(Font.SERIF, Font.PLAIN, 20);
+					m_subtitle.setFont(f);
+					m_subtitle.setHorizontalAlignment(SwingConstants.CENTER);
+					m_subtitleFrame.add(m_subtitle);
 	
-	@Override
-	public void setSubtitle(String subtitle, double displayTimeInSecond) {
-		m_subtitle.setText("<html><body>" + subtitle + "</body></html>");
-		m_displayTimeInSec = displayTimeInSecond;
-		m_startTime = System.currentTimeMillis();
-		m_subtitleFrame.toFront();
-		m_subtitleFrame.setVisible(true);
+					m_subtitleFrame.pack();
+				}
+			});
+		}
+		catch (Exception e) {
+            throw new QTasteException("Error while creating subtitler window", e);
+		}
 	}
 
 	/**
-	 * while the thread is needed, this method checks every second if the subtitle frame has to be hidden or not. 
+	 * Initialize the execution of the subtitler.
+	 * @throws QTasteException
+	 */
+	@Override
+	public void initialize() throws QTasteException {
+		m_abort = false;
+
+		m_thread = new Thread(this);
+		m_thread.start();		
+	}
+
+	/**
+	 * Stop the execution of the subtitler.
+	 * @throws QTasteException
+	 */
+	@Override
+	public void terminate() throws QTasteException {
+		m_abort = true;
+	}
+
+	/**
+	 * Set the current subtitle.
+	 * @param subtitle the new subtitle
+	 * @throws QTasteException
+	 */
+	@Override
+	public void setSubtitle(String subtitle) throws QTasteException {
+		setSubtitle(subtitle, 3);
+	}
+	
+	/**
+	 * Set the current subtitle and the display time.
+	 * @param subtitle the new subtitle
+	 * @param displayTimeInSecond the time while the subtitle is displayed.
+	 * @throws QTasteException
+	 */
+	@Override
+	public void setSubtitle(final String subtitle, final double displayTimeInSecond) throws QTasteException {
+		
+		try {
+			SwingUtilities.invokeAndWait(new Runnable() {
+				public void run() {
+					m_subtitle.setText("<html><body>" + subtitle + "</body></html>");
+		
+					System.out.println("subtitle:" + m_subtitle.getText());
+
+					m_displayTimeInSec = displayTimeInSecond;
+					m_startTime = System.currentTimeMillis();
+					m_subtitleFrame.toFront();
+					m_subtitleFrame.setVisible(true);
+				}
+			});
+		}
+		catch (Exception e) {
+            throw new QTasteException("Error while updating subtitler text", e);
+		}
+	}
+
+	/**
+	 * while the thread is needed, this method checks every second 
+	 * if the subtitle frame has to be hidden or not. 
 	 */
 	public void run() {
-		while ( !m_abort )
-		{
-			if ( m_subtitleFrame.isVisible() )
-			{
-				if (System.currentTimeMillis() > (m_startTime + m_displayTimeInSec*1000) )
-				{
-					m_subtitleFrame.setVisible(false);
+		
+		while ( !m_abort ) {
+			
+			if ( m_subtitleFrame.isVisible() ) {
+				if (System.currentTimeMillis() > (m_startTime + m_displayTimeInSec * 1000) ) {
+					setVisible(false);
 				}
 			}
-			try{
+			
+			try {
 				Thread.sleep(1000);
-			} catch (InterruptedException e) {
+			}
+			catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
-		m_subtitleFrame.setVisible(false);
+		
+		setVisible(false);
+	}
+
+	/**
+	 * Change the subtitler window visibility in the EDT.
+	 * @param the new visibility state.
+	 */
+	private void setVisible(final boolean state) {
+		try {
+			SwingUtilities.invokeAndWait(new Runnable() {
+				public void run() {
+					m_subtitleFrame.setVisible(state);
+				}
+			});
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
-	private final JWindow m_subtitleFrame = new JWindow();
-	private JLabel m_subtitle;
-	private double m_displayTimeInSec;
-	private long m_startTime;
-	private Thread m_thread;
+	private JWindow m_subtitleFrame;
+	private JLabel  m_subtitle;
+	private double  m_displayTimeInSec;
+	private long 	m_startTime;
+	private Thread  m_thread;
 	private boolean m_abort = false;
 }
