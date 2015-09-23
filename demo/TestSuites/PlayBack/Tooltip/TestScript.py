7921bfa036a09b4f8c85005cf2c2b867eaa9e462
simjan <janssenssimonjc@gmail.com>
2015-09-23 13:57:55 +0200

    DEMO - replace tab by spaces

--- demo/TestSuites/PlayBack/Tooltip/TestScript.py
+++ demo/TestSuites/PlayBack/Tooltip/TestScript.py
@@ -13,15 +13,15 @@ from qtaste import *
 javaGUI = testAPI.getJavaGUI(INSTANCE_ID=testData.getValue("JAVAGUI_INSTANCE_NAME"))
 
 def checkToolTip():
-	"""
-	@step      Check the component tooltip is the expected one.
-	@expected  The component's tooltip is the same as the one from testData
-	"""
-	current = javaGUI.getToolTip(testData.getValue("COMPONENT_ID"))
-	expected = testData.getValue("EXPECTED_TOOLTIP")
-	if current != expected:
-		testAPI.stopTest(Status.FAIL, "The returned tooltip (" + current + ") is not the expected one (" + expected + ")...")
-	pass
+    """
+    @step      Check the component tooltip is the expected one.
+    @expected  The component's tooltip is the same as the one from testData
+    """
+    current = javaGUI.getToolTip(testData.getValue("COMPONENT_ID"))
+    expected = testData.getValue("EXPECTED_TOOLTIP")
+    if current != expected:
+        testAPI.stopTest(Status.FAIL, "The returned tooltip (" + current + ") is not the expected one (" + expected + ")...")
+    pass
 
 javaGUI.selectTabId("TABBED_PANE", "DOCUMENT_PANEL")
 doStep(checkToolTip)
