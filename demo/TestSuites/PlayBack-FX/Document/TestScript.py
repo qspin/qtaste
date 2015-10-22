2e827bee0836430d358d9971db3059dfa9be88a9
simjan <janssenssimonjc@gmail.com>
2015-10-22 14:45:53 +0200

    test for java fx gui control validation

---
+++ demo/TestSuites/PlayBack-FX/Document/TestScript.py
@@ -0,0 +1,55 @@
+# encoding= utf-8
+
+##
+# Playback/Document test.
+# <p>
+# Description of the test.
+#
+##
+
+from qtaste import *
+
+
+import time
+
+# update in order to cope with the javaGUI extension declared in your testbed configuration.
+javaguiMI = testAPI.getJavaGUIFX(INSTANCE_ID=testData.getValue("JAVAGUI_INSTANCE_NAME"))
+subtitler = testAPI.getSubtitler()
+
+def step1():
+    """
+    @step      Description of the actions done for this step
+    @expected  Description of the expected result
+    """
+
+    javaguiMI.selectTabId("TABBED_PANE", "DOCUMENT_PANEL")
+    subtitler.setSubtitle(testData.getValue("COMMENT") + " the value '" + testData.getValue("VALUE") + "'")
+    time.sleep(1)
+    text = testData.getValue("VALUE")
+    component = testData.getValue("COMPONENT_NAME")
+    try:
+        javaguiMI.setText(component, text) != testData.getBooleanValue("COMMAND_RESULT")
+    except:
+        testAPI.stopTest(Status.FAIL, "Fail to insert " + text)
+
+    time.sleep(1)
+
+    if testData.getBooleanValue("COMMAND_RESULT"):
+        result = javaguiMI.getText(component)
+        text = testData.getValue("FIELD_CONTENT")
+        if result != text:
+            testAPI.stopTest(Status.FAIL, "Fail to insert " + text + " but insert '" + result + "'")
+
+
+def reset():
+    """
+    @step      Reset component state
+    @expected  Description of the expected result
+    """
+
+    component = testData.getValue("COMPONENT_NAME")
+    value = ""
+    javaguiMI.setText(component, value)
+
+doStep(step1)
+doStep(reset)
---
+++ demo/TestSuites/PlayBack-FX/Selection/TestScript.py
@@ -0,0 +1,72 @@
+# encoding= utf-8
+
+##
+# Playback/Selection test.
+# <p>
+# Description of the test.
+#
+##
+
+from qtaste import *
+
+
+import time
+
+# update in order to cope with the javaGUI extension declared in your testbed configuration.
+javaguiMI = testAPI.getJavaGUI(INSTANCE_ID=testData.getValue("JAVAGUI_INSTANCE_NAME"))
+subtitler = testAPI.getSubtitler()
+
+def step1():
+    """
+    @step      Description of the actions done for this step
+    @expected  Description of the expected result
+    """
+
+    javaguiMI.selectTabId("TABBED_PANE", testData.getValue("TAB_ID"))
+    subtitler.setSubtitle(testData.getValue("COMMENT"))
+    component = testData.getValue("COMPONENT_NAME")
+    value = testData.getIntValue("INDEX")
+    if value != -1:
+        try:
+            javaguiMI.selectIndex(component, value)
+        except:
+            testAPI.stopTest(Status.FAIL, "Fail to select index " + testData.getValue("INDEX") + " in " + component + "'")
+    else:
+        value = testData.getValue("VALUE")
+        try:
+            javaguiMI.selectValue(component, value)
+        except:
+            testAPI.stopTest(Status.FAIL, "Fail to select value '" + value + "' in " + component + "'")
+        actualSelection = javaguiMI.getSelectedValue(component)
+        if value != actualSelection:
+            testAPI.stopTest(Status.FAIL, "Expected to see '" + value + "' selected in '" + component + "' but got '" + actualSelection + "'")
+
+    time.sleep(1)
+
+def checkList():
+    """
+    @step      Check the content of the list
+    @expected  Description of the expected result
+    """
+    actualList = javaguiMI.getListContent("LIST")
+    if len(actualList) != 5:
+        testAPI.stopTest(Status.FAIL, "Expected to get 5 elements in the list but got %d" %(len(actualList)))
+    expectedList = ["Mickey Mouse (70)", "Tintin Milou (40)", "Louis XVII (30)", "Elisabeth II (80)", "Milou Tintin (40)"]
+    i = 0
+    for e in expectedList:
+        if actualList[i] != expectedList[i]:
+            testAPI.stopTest(Status.FAIL, "Expected to get %s at indice %d in the list but got %s" %(expectedList[i], i, actualList[i]))
+        i = i + 1
+
+
+def reset():
+    """
+    @step      Reset component state
+    @expected  Description of the expected result
+    """
+
+    component = testData.getValue("COMPONENT_NAME")
+
+doStep(step1)
+doStep(checkList)
+doStep(reset)
