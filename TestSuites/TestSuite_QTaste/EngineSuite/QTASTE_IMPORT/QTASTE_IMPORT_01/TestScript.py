fcff2a9845cd2b2e224c26cb42a720c5c8e7a13b
simjan <janssenssimonjc@gmail.com>
2015-10-27 11:27:32 +0100

    issue #92 - fix documentation warning

--- TestSuites/TestSuite_QTaste/EngineSuite/QTASTE_IMPORT/QTASTE_IMPORT_01/TestScript.py
+++ TestSuites/TestSuite_QTaste/EngineSuite/QTASTE_IMPORT/QTASTE_IMPORT_01/TestScript.py
@@ -1,6 +1,6 @@
 # encoding= utf-8
 
-##
+#
 #    Copyright 2007-2009 QSpin - www.qspin.be
 #
 #    This file is part of QTaste framework.
@@ -17,24 +17,31 @@
 #
 #    You should have received a copy of the GNU Lesser General Public License
 #    along with QTaste. If not, see <http://www.gnu.org/licenses/>.
-##
+#
 
 from qtaste import *
 from lib import *
 
+##
+#  QTaste documentation management: 
+# <p>
+# This test case has the goal to verify that  when a test step is imported, tis documentation is well imported too.
+##
+
+
 def Step2():
-	"""
-	@step Define a Python script containing the following a syntax error: <p>
-		  if myValue eq True:
-	@expected Test result is "Not available" with the following reason:<p>
-			  <i>Python syntax errorin file Testcase.py at line 18, column 20:
-			  if myValue eq True:</i><p>
-			  Script call stack is reported.
-	"""
-	if myValue eq True:
-		print "myValue is true"
-	else:	
-		print "myValue is false"
+    """
+    @step Define a Python script containing the following a syntax error: <p>
+          if myValue eq True:
+    @expected Test result is "Not available" with the following reason:<p>
+              <i>Python syntax errorin file Testcase.py at line 18, column 20:
+              if myValue eq True:</i><p>
+              Script call stack is reported.
+    """
+    if myValue eq True:
+        print "myValue is true"
+    else:    
+        print "myValue is false"
 
 doStep(Step1FromLIB)
 doStep(Step2)
