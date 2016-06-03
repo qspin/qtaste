9aed3afb18a58f26ee9305f9fe207069981f80fd
David Ergo <david.ergo@iba-group.com>
2016-06-03 12:19:42 +0200

    Fixed UTF-8 exception in syntax checker

--- TestSuites/TestSuite_QTaste/EngineSuite/QTASTE_REPORT/QTASTE_REPORT_03/TestScript.py
+++ TestSuites/TestSuite_QTaste/EngineSuite/QTASTE_REPORT/QTASTE_REPORT_03/TestScript.py
@@ -1,4 +1,4 @@
-# encoding= utf-8
+# coding=utf-8
 
 #    Copyright 2007-2009 QSpin - www.qspin.be
 #
--- kernel/src/main/java/com/qspin/qtaste/util/ScriptCheckSyntaxValidator.java
+++ kernel/src/main/java/com/qspin/qtaste/util/ScriptCheckSyntaxValidator.java
@@ -38,106 +38,124 @@ public class ScriptCheckSyntaxValidator {
     //static private Logger logger = Log4jLoggerFactory.getLogger(ScriptCheckSyntaxValidator.class);
     private String mScriptContent;
     private String mFileName;
-    private static Pattern mDoStepPattern = Pattern.compile("doStep\\(\\s*['\"]?([\\w.]+)['\"]?(?:\\s*,\\s*([\\w.]+))?\\s*\\)");
-    //private static Pattern mDoStepsPattern = Pattern.compile("doSteps\\(\\s*([\\w.]+)(?:\\s*,\\s*['\"]\\s*\\[(\\w*)(?:\\s*-\\s*(\\w*))?\\]\\s*['\"]\\s*)?\\s*\\)");
-	private static Pattern mStepsTableDefPattern = Pattern.compile("(\\w+)\\s*=\\s*[\\(\\[]\\s*(\\(\\s*['\"]?\\w+['\"]?\\s*,\\s*\\w+\\s*\\)(?:\\s*,\\s*\\(\\s*['\"]?\\w+['\"]?\\s*,\\s*\\w+\\s*\\))*)\\s*[\\)\\]]");
-    
+    private static final Pattern PEP_263_ENCODING_PATTERN = Pattern.compile("#.*coding[:=]\\s*([-\\w.]+)");
+    private static final Pattern DO_STEP_PATTERN = Pattern.compile(
+          "doStep\\(\\s*['\"]?([\\w.]+)['\"]?(?:\\s*,\\s*([\\w.]+))?\\s*\\)");
+    //private static final Pattern DO_STEPS_PATTERN = Pattern.compile("doSteps\\(\\s*([\\w.]+)(?:\\s*,\\s*['\"]\\s*\\[(\\w*)
+    // (?:\\s*-\\s*(\\w*))?\\]\\s*['\"]\\s*)?\\s*\\)");
+    private static final Pattern STEPS_TABLE_DEF_PATTERN = Pattern.compile(
+          "(\\w+)\\s*=\\s*[\\(\\[]\\s*(\\(\\s*['\"]?\\w+['\"]?\\s*,\\s*\\w+\\s*\\)(?:\\s*,\\s*\\(\\s*['\"]?\\w+['\"]?\\s*,"
+                + "\\s*\\w+\\s*\\))*)\\s*[\\)\\]]");
+
     public ScriptCheckSyntaxValidator(String fileName, String scriptContent) {
         mScriptContent = scriptContent;
         mFileName = fileName;
     }
-    
+
     public boolean check() {
         ScriptEngineManager manager = new ScriptEngineManager();
         ScriptEngine engine = manager.getEngineByName("python");
         Compilable compilable = (Compilable) engine;
 
+        // work-around for Jython ParserFacade.prepBufReader() bug which sets encoding to UTF-8 but fails when encoding is defined
+        String scriptContent = PEP_263_ENCODING_PATTERN.matcher(mScriptContent).replaceFirst("");
+
         try {
-			compilable.compile(mScriptContent);
-	        checkStepsDescriptions();
-	        return true;
-		} catch (ScriptException e) {
+            compilable.compile(scriptContent);
+            checkStepsDescriptions();
+            return true;
+        } catch (ScriptException e) {
             JOptionPane.showMessageDialog(null, "Syntax error in file '" + mFileName + "':\n" + e.getMessage());
             return false;
-		} catch (FunctionDocumentationException e) {
+        } catch (FunctionDocumentationException e) {
             JOptionPane.showMessageDialog(null, "Step documentation error in file '" + mFileName + "':\n" + e.getMessage());
             return false;
-		}
+        }
     }
-    
-    private void checkStepsDescriptions() throws FunctionDocumentationException {
-    	for (String stepFunctionName: getStepsFunctionsNames()) {
-    		try {
-				String stepFunctionDoc = getFunctionDoc(stepFunctionName);
-				HashMap<String, String> stepDocTags = JythonTestScript.parsePythonDoc(stepFunctionDoc);
-	            String stepDescription = stepDocTags.get("step");
-	            if (stepDescription == null) {
-	            	throw new FunctionDocumentationException("Function " + stepFunctionName + " documentation contains no @step tag");
-	            } else if (stepDescription.length() == 0) {
-	            	throw new FunctionDocumentationException("Function " + stepFunctionName + " documentation contains no description for the @step tag");	            	
-	            }
-			} catch (FunctionDefinitionNotFoundException e) {
-				// do nothing because we suppose step definition is imported
-			}
-    	}
-    }
-    
-    private List<String> getStepsFunctionsNames() {
-    	List<String> stepsFunctionsNames = new ArrayList<String>(); 
 
-    	Matcher doStepMatcher = mDoStepPattern.matcher(mScriptContent);
-    	while (doStepMatcher.find()) {
-    		String stepFunctionName = (doStepMatcher.group(2) != null ? doStepMatcher.group(2) : doStepMatcher.group(1));
-    		stepsFunctionsNames.add(stepFunctionName);
-    	}
-    	
-    	Matcher stepsTableDefMatcher = mStepsTableDefPattern.matcher(mScriptContent);
-    	while (stepsTableDefMatcher.find()) {
-    		String[] stepFunctionNamesAndIds = stepsTableDefMatcher.group(2).split("\\W+");
-    		for (int i=2; i < stepFunctionNamesAndIds.length; i+=2) {
-    			stepsFunctionsNames.add(stepFunctionNamesAndIds[i]);
-    		}
-    	}
-    	
-    	return stepsFunctionsNames;
+    private void checkStepsDescriptions() throws FunctionDocumentationException {
+        for (String stepFunctionName : getStepsFunctionsNames()) {
+            try {
+                String stepFunctionDoc = getFunctionDoc(stepFunctionName);
+                HashMap<String, String> stepDocTags = JythonTestScript.parsePythonDoc(stepFunctionDoc);
+                String stepDescription = stepDocTags.get("step");
+                if (stepDescription == null) {
+                    throw new FunctionDocumentationException(
+                          "Function " + stepFunctionName + " documentation contains no @step tag");
+                }
+                else if (stepDescription.length() == 0) {
+                    throw new FunctionDocumentationException(
+                          "Function " + stepFunctionName + " documentation contains no description for the @step tag");
+                }
+            } catch (FunctionDefinitionNotFoundException e) {
+                // do nothing because we suppose step definition is imported
+            }
+        }
     }
-    
-    private String getFunctionDoc(String functionName) throws FunctionDefinitionNotFoundException, FunctionDocumentationException {
-    	Pattern functionDefinitionPattern = Pattern.compile("^def\\s+" + functionName + "\\(\\)\\s*:\\s*$", Pattern.MULTILINE);
-    	Matcher functionDefinitionMatcher = functionDefinitionPattern.matcher(mScriptContent);
-    	if (functionDefinitionMatcher.find()) {
-    		Pattern notInFunctionAnymorePattern = Pattern.compile("^\\S", Pattern.MULTILINE);
-    		Matcher notInFunctionAnymoreMatcher = notInFunctionAnymorePattern.matcher(mScriptContent);
-    		String functionDefinition;
-    		if (notInFunctionAnymoreMatcher.find(functionDefinitionMatcher.end())) {
-    			functionDefinition = mScriptContent.substring(functionDefinitionMatcher.end() + 1, notInFunctionAnymoreMatcher.start() - 1);
-    		} else {
-    			functionDefinition = mScriptContent.substring(functionDefinitionMatcher.end() + 1);
-    		}
-    		
-    		Pattern docStringPattern = Pattern.compile("^\\s*('''|\"\"\")(.*)\\1", Pattern.DOTALL);
-    		Matcher docStringMatcher = docStringPattern.matcher(functionDefinition);
-    		if (docStringMatcher.find()) {
-    			return docStringMatcher.group(2);
-    		} else {
-    			throw new FunctionDocumentationException("Function " + functionName + " is not documented by a docstring");
-    		}
-    	} else {
-    		throw new FunctionDefinitionNotFoundException("Definition of function " + functionName + " not found");
-    	}
-    }    
-    
-    @SuppressWarnings("serial")
-	private class FunctionDefinitionNotFoundException extends Exception {
-    	public FunctionDefinitionNotFoundException(String message) {
-    		super(message);
-    	}
+
+    private List<String> getStepsFunctionsNames() {
+        List<String> stepsFunctionsNames = new ArrayList<String>();
+
+        Matcher doStepMatcher = DO_STEP_PATTERN.matcher(mScriptContent);
+        while (doStepMatcher.find()) {
+            String stepFunctionName = (doStepMatcher.group(2) != null ? doStepMatcher.group(2) : doStepMatcher.group(1));
+            stepsFunctionsNames.add(stepFunctionName);
+        }
+
+        Matcher stepsTableDefMatcher = STEPS_TABLE_DEF_PATTERN.matcher(mScriptContent);
+        while (stepsTableDefMatcher.find()) {
+            String[] stepFunctionNamesAndIds = stepsTableDefMatcher.group(2).split("\\W+");
+            for (int i = 2;
+                  i < stepFunctionNamesAndIds.length;
+                  i += 2) {
+                stepsFunctionsNames.add(stepFunctionNamesAndIds[i]);
+            }
+        }
+
+        return stepsFunctionsNames;
     }
-    
+
+    private String getFunctionDoc(String functionName) throws FunctionDefinitionNotFoundException,
+          FunctionDocumentationException {
+        Pattern functionDefinitionPattern = Pattern.compile("^def\\s+" + functionName + "\\(\\)\\s*:\\s*$", Pattern.MULTILINE);
+        Matcher functionDefinitionMatcher = functionDefinitionPattern.matcher(mScriptContent);
+        if (functionDefinitionMatcher.find()) {
+            Pattern notInFunctionAnymorePattern = Pattern.compile("^\\S", Pattern.MULTILINE);
+            Matcher notInFunctionAnymoreMatcher = notInFunctionAnymorePattern.matcher(mScriptContent);
+            String functionDefinition;
+            if (notInFunctionAnymoreMatcher.find(functionDefinitionMatcher.end())) {
+                functionDefinition = mScriptContent.substring(functionDefinitionMatcher.end() + 1,
+                      notInFunctionAnymoreMatcher.start() - 1);
+            }
+            else {
+                functionDefinition = mScriptContent.substring(functionDefinitionMatcher.end() + 1);
+            }
+
+            Pattern docStringPattern = Pattern.compile("^\\s*('''|\"\"\")(.*)\\1", Pattern.DOTALL);
+            Matcher docStringMatcher = docStringPattern.matcher(functionDefinition);
+            if (docStringMatcher.find()) {
+                return docStringMatcher.group(2);
+            }
+            else {
+                throw new FunctionDocumentationException("Function " + functionName + " is not documented by a docstring");
+            }
+        }
+        else {
+            throw new FunctionDefinitionNotFoundException("Definition of function " + functionName + " not found");
+        }
+    }
+
     @SuppressWarnings("serial")
-	private class FunctionDocumentationException extends Exception {
-    	public FunctionDocumentationException(String message) {
-    		super(message);
-    	}
+    private class FunctionDefinitionNotFoundException extends Exception {
+        public FunctionDefinitionNotFoundException(String message) {
+            super(message);
+        }
+    }
+
+    @SuppressWarnings("serial")
+    private class FunctionDocumentationException extends Exception {
+        public FunctionDocumentationException(String message) {
+            super(message);
+        }
     }
 }
