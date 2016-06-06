ce103e1a9b2f07f0ff941189ea8f18d1b8e0fab5
lvboque <laurent.vanboquestal@gmail.com>
2016-06-06 10:24:04 +0200

    Fix the build.cmd script

--- demo/build.cmd
+++ demo/build.cmd
@@ -26,4 +26,16 @@ if "%FAILED%" == "1" pause
 
 popd
 
+pushd SUT-FX
+
+set PATH=%PATH%;%QTASTE_ROOT%\tools\GnuWin32\bin
+
+rem build using maven
+call mvn clean install assembly:single || set FAILED=1
+
+rem pause if build failed
+if "%FAILED%" == "1" pause
+
+popd
+
 endlocal
